82bef65ef6d14e1ebc8487bfc23023d4acd5ae89
Tigraine <tigraine@tigraine.at>
2009-08-22 14:13:33 +0200

    Created thin Wrapper around LessCss through IronRuby and wrote Buildfile to extract IronRuby and Ruby through 7zip to /buid folder

---
+++ src/IronLess.Compiler/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace IronLess.Compiler
+{
+    using System;
+    using System.Diagnostics;
+    using Wrapper;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please specify less filename to compile to css");
+                return;
+            }
+            string inputFilename = args[0];
+            string outputFileName = "";
+            Console.WriteLine("Compiling {0} to {1}", inputFilename, outputFileName);
+            var elapsedTime = CountMilliseconds(() =>
+                      new IronLessExecuter().CompileLess(inputFilename)
+                );
+
+            Console.WriteLine("Compilation finished after {0} seconds", elapsedTime);
+        }
+
+        private delegate void ExecutingAction();
+
+        private static long CountMilliseconds(ExecutingAction method)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            method();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+    }
+}
---
+++ src/IronLess.Wrapper/IronLessExecuter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronLess.Wrapper
+{
+    using System.Diagnostics;
+    using Microsoft.Scripting.Hosting;
+
+    public class IronLessExecuter
+    {
+        public void CompileLess(string fileName)
+        {
+            ScriptEngine engine = IronRuby.Ruby.CreateEngine();
+            engine.SetSearchPaths(new List<string>()
+                                      {
+                                          @".\IronRuby",
+                                          @".\ruby\site_ruby\1.8",
+                                          @".\ruby\1.8",
+                                          //@".\lib\\IronRuby\gems\1.8\gems\less-1.1.13\lib\vendor\treetop\lib",
+                                          @".\IronRuby\gems\1.8\gems\less-1.1.13\lib\"
+                                      });
+            engine.Execute(
+                @"require 'less'
+options = {
+  :watch => false,
+  :compress => false,
+  :debug => true,
+  :growl => false
+}
+options[:source] = 'variables.less'
+Less::Command.new( options ).run!");
+        }
+    }
+}
